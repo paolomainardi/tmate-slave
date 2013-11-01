1281bf197c0c25757115c781d8dbe0fc4630a6f6
Paolo Mainardi <paolo@twinbit.it>
2013-11-01 18:48:50 +0100

    fix

--- tmate-decoder.c
+++ tmate-decoder.c
@@ -120,13 +120,13 @@ static void tmate_header(struct tmate_decoder *decoder,
 		tmate_fatal("cannot get hostname");
 
 	tmate_notify("Remote session read only: ssh %d ro-%s@%s.%s (clear your screen if you share this)",
-				 (TMATE_DEFAULT_PORT != 22 ? '' : TMATE_DEFAULT_PORT),
+				 (TMATE_DEFAULT_PORT !== 22 ? '' : TMATE_DEFAULT_PORT),
 		     tmate_session_token_ro,
 		     hostname,
 		     TMATE_DOMAIN);
 
 	tmate_notify("Remote session: ssh %d %s@%s.%s",
-				 (TMATE_DEFAULT_PORT != 22 ? '' : TMATE_DEFAULT_PORT),
+				 (TMATE_DEFAULT_PORT !== 22 ? '' : TMATE_DEFAULT_PORT),
 		     tmate_session_token,
 		     hostname,
 		     TMATE_DOMAIN);
